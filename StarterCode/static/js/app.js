097ba6e65d908838f83677dbed86e265c1ef65f4
owenmeyer3 <owenmeyer3@gmail.com>
2021-01-11 17:12:06 -0600

    Worked on bar chart for single subject

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,12 +1,63 @@
+// select tags
+let selDataSetTag = d3.select('#selDataset')
+let data = [];
+let otu_ids = [];
+let sample_values = [];
+let otu_labels = [];
+let selectId = 0;
+
 // Get data
-d3.json('../data/samples.json').then(data => {
+d3.json('../data/samples.json').then(dataIn => {
     // log data
-    console.log(data);
-
-    d3.select('#selDataset')
+    console.log(dataIn);
+    data = dataIn;
+    selDataSetTag
         .selectAll('option')
-        .data(data.samples).enter()
+        .data(dataIn.samples).enter()
         .append('option')
-        .html( d => `${d.id}`)
-
+        .html( d => `${d.id}`);
+    // get initial id
+    selectId = selDataSetTag.node().value;
+    console.log(selectId);
+    optionChanged(selectId);
 });
+
+function optionChanged(id){ 
+    let selectedId = id;
+    data.samples.forEach( row => {
+        if(row.id === selectedId){
+            otu_ids = row.otu_ids;
+            sample_values = row.sample_values;
+            otu_labels = row.otu_labels;
+        };
+    });
+    console.log('otu_ids: ', otu_ids);
+    console.log('sample_values: ', sample_values);
+    console.log('otu_labels: ', otu_labels);
+    createBarChart(selectedId, otu_ids, otu_labels, sample_values);
+};
+
+function createBarChart(id, otu_ids, otu_labels, sample_values){
+    let otu_ids_text = otu_ids.map(id => id.toString());
+    var trace1 = {
+        x: otu_ids_text,
+        y: sample_values,
+        text: otu_labels,
+        type: 'bar',
+    };
+
+    var traces = [trace1];
+    var layout = {
+        title: `Bacteria Counts in Subject ${id}`,
+        height: '100%',
+        width: '100%',
+        xaxis: {title: 'Bacteria Type', automargin: true, type: 'category'},
+        yaxis: {title: 'Bacteria Count', automargin: true}
+    };
+
+    Plotly.newPlot(
+        'bar',
+        traces,
+        layout
+    );
+};
